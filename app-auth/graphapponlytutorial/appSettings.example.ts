88cc31898ca5528f11f25309ccd0d6a0d67073a4
Jason Johnston <jasonjoh@users.noreply.github.com>
2022-11-10 11:58:40 -0500

    Update appSettings.example.ts

--- app-auth/graphapponlytutorial/appSettings.example.ts
+++ app-auth/graphapponlytutorial/appSettings.example.ts
@@ -1,10 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
+// <SettingsSnippet>
 const settings: AppSettings = {
   'clientId': 'YOUR_CLIENT_ID_HERE',
   'clientSecret': 'YOUR_CLIENT_SECRET_HERE',
-  'tenantId': 'common'
+  'tenantId': 'YOUR_TENANT_ID_HERE'
 };
 
 export interface AppSettings {
@@ -14,3 +15,4 @@ export interface AppSettings {
 }
 
 export default settings;
+// <SettingsSnippet>
